abf34b37ec953211200fa1b8e8bfa97c2bac7295
Brandon Bachrach <brandon.bachrach@gmail.com>
2021-12-18 13:25:20 -0500

    Remove SQL queries for security:

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.post(
     try {
       await sql
         .raw(
-          `exec xct_spOCEditsubscription @custID=N'${req.query.custID}',@notificationEmail=N'${req.query.notificationEmail}',@frequencyType=N'${freq}',@user9=0,@user8='1900-01-01 00:00:00',@subscriptionGUID=N'${req.query.subscriptionGUID}',@shipViaID=N'${req.query.shipViaID}',@userID=N'${req.query.userID}',@lastUpdated='${req.query.lastUpdated}',@nextShipDate='${nextShipDate}',@catalogID=N'${req.query.catalogID}',@lastShipDate='${req.query.lastShipDate}',@active=${active},@actionType=N'UPDATE',@paymentMethodGUID=N'${paymentMethodGUID}',@user11=N'',@createdDate='${req.query.createdDate}',@shipToID=N'${address}',@user5=N'',@user7='1900-01-01 00:00:00',@user6=N'',@user10=0,@user4=0,@user3=0,@user2=N'',@user1=N''`
+          `REDACTED FOR SECURITY`
         )
         .then((res) => console.log(res));
       res.end("Successfully Updated Subscription");
@@ -74,9 +74,7 @@ app.get(
   async (req, res) => {
     try {
       const paymentMethods = await sql.raw(
-        `SELECT paymentMethodGUID, paymentMethodName, paymentMethodDescr, expiration
-        FROM xct_tblOCuserPaymentMethod
-        WHERE CustId = '${req.query.custID}'`
+        `REDACTED FOR SECURITY`
       );
       res.json({ paymentMethods });
     } catch (err) {
@@ -88,15 +86,7 @@ app.get(
 app.get("/get-subscription-details", cors(corsOptions), async (req, res) => {
   try {
     const subscriptionDetails = await sql.raw(
-      `SELECT 
-            subs.*,
-            subsdet.itemID, subsdet.invtID, subsdet.qtyOrd, subsdet.price,
-            items.Name, Items.Descr, Items.User1 AS itemURL
-        FROM xct_tblOCsubscription subs with(nolock)
-        JOIN xct_tblOCsubscriptionDetail subsdet with(nolock) ON subs.subscriptionGUID = subsdet.subscriptionGUID
-        JOIN xCTItems items with(nolock) ON subsdet.itemID = items.ItemID
-        WHERE subs.subscriptionGUID = '${req.query.subscriptionGUID}'
-        ORDER BY subs.catalogID, items.Name ASC`
+      `REDACTED FOR SECURITY`
     );
     res.json({ subscriptionDetails });
   } catch (err) {
@@ -107,15 +97,7 @@ app.get("/get-subscription-details", cors(corsOptions), async (req, res) => {
 app.get("/get-customer-subscriptions", cors(corsOptions), async (req, res) => {
   try {
     const customerSubs = await sql.raw(
-      `SELECT 
-            subs.CustId, subs.catalogID, subs.subscriptionGUID, subs.active, subs.frequencyType, subs.nextShipDate,
-            subsdet.itemID, subsdet.price,
-            items.Name, Items.Descr
-        FROM xct_tblOCsubscription subs with(nolock)
-        JOIN xct_tblOCsubscriptionDetail subsdet with(nolock) ON subs.subscriptionGUID = subsdet.subscriptionGUID
-        JOIN xCTItems items with(nolock) ON subsdet.itemID = items.ItemID
-        WHERE subs.CustId = '${req.query.custID}'
-        ORDER BY subs.catalogID, items.Name ASC`
+      `REDACTED FOR SECURITY`
     );
     res.json({ customerSubs });
   } catch (err) {
@@ -126,11 +108,7 @@ app.get("/get-customer-subscriptions", cors(corsOptions), async (req, res) => {
 app.get("/get-customer-details", cors(corsOptions), async (req, res) => {
   try {
     const customerDetails = await sql.raw(
-      `SELECT DISTINCT
-        LTRIM(RTRIM(CustId)) as CustId, LTRIM(RTRIM(users.Name)) AS CustName, LTRIM(RTRIM(users.addr1)) AS addr1, LTRIM(RTRIM(users.Addr2)) AS Addr2, LTRIM(RTRIM(users.City)) AS City, LTRIM(RTRIM(users.State)) AS State, LTRIM(RTRIM(users.Zip)) AS Zip, LTRIM(RTRIM(users.Country)) AS Country, LTRIM(RTRIM(users.EMailAddr)) AS EMailAddr, LTRIM(RTRIM(users.ShipToId)) AS ShipToId
-        FROM SOAddress users with(nolock)
-        WHERE users.CustId = '${req.query.custID}'
-        ORDER BY CustName ASC`
+      `REDACTED FOR SECURITY`
     );
     res.json({ customerDetails });
   } catch (err) {
@@ -138,16 +116,10 @@ app.get("/get-customer-details", cors(corsOptions), async (req, res) => {
   }
 });
 
-// Check if we should not check for active subs.
 app.get("/get-active-subscribers", cors(corsOptions), async (req, res) => {
   try {
     const activeSubscribers = await sql.raw(
-      `SELECT DISTINCT
-          LTRIM(RTRIM(subs.catalogID)) AS CatID, LTRIM(RTRIM(users.CustId)) AS CustID, LTRIM(RTRIM(users.Name)) AS Name
-        FROM SOAddress users with(nolock)
-        JOIN xct_tblOCsubscription subs with(nolock) ON users.custID = subs.custID
-        WHERE subs.active = 1
-        ORDER BY Name ASC`
+      `REDACTED FOR SECURITY`
     );
     res.json({ activeSubscribers });
   } catch (err) {
